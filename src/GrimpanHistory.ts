71ba4b77402590a43257c5ebfe259321b5d4fbc9
mnmn092631 <mnmn092631@gmail.com>
2025-02-21 19:08:03 +0900

    프로토타입(Prototype) - 기존 객체를 복붙해서 새 객체 만들기

--- src/GrimpanHistory.ts
+++ src/GrimpanHistory.ts
@@ -2,10 +2,29 @@ import Grimpan from "./AbstractGrimpan.js";
 import IEGrimpan from "./IEGrimpan.js";
 import ChromeGrimpan from "./ChromeGrimpan.js";
 
+interface Clonable {
+  clone(): Clonable;
+}
+
+class HistoryStack extends Array implements Clonable {
+  clone() {
+    return this.slice() as HistoryStack;
+  }
+}
+
 export abstract class GrimpanHistory {
   grimpan: Grimpan;
+  stack: HistoryStack;
   protected constructor(grimpan: Grimpan) {
     this.grimpan = grimpan;
+    this.stack = new HistoryStack();
+  }
+
+  getStack() {
+    return this.stack.clone();
+  }
+  setStack(stack: HistoryStack) {
+    this.stack = stack.clone();
   }
 
   abstract initialize(): void;
